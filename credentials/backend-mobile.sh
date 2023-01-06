dc5dc50127b380357f6057b332f689c06784de91
0x000001A4 <ricky420@ricky>
2023-01-06 12:17:51 +0000

    Sidnei toma 3

--- credentials/backend-mobile.sh
+++ credentials/backend-mobile.sh
@@ -2,7 +2,7 @@
 
 # Mobile side
 echo "Creating Server's private key and Certificate Signing Request"
-openssl req -newkey rsa:4096 -nodes -keyout mobile-server-key.pem -out mobile-server-req.pem -subj "/C=PT/ST=Lisbon/L=Oeiras/O=Lemon/OU=Lemon/CN=192.168.2.2/emailAddress=lemon@gmail.pt"
+openssl req -newkey rsa:4096 -nodes -keyout mobile-server-key.pem -out mobile-server-req.pem -subj "/C=PT/ST=Lisbon/L=Oeiras/O=Lemon/OU=Lemon/CN=192.168.2.2/emailAddress=lemon@gmail.pt" -addtext "subjectAltName = IP:192.168.2.2"
 
 echo "Signing server certificate request with ca's private key"
 openssl x509 -req -in mobile-server-req.pem -CA ca-cert.pem -CAkey ca-key.pem -CAcreateserial -out mobile-server-cert.pem
@@ -12,7 +12,7 @@ openssl x509 -in mobile-server-cert.pem -noout -text
 
 # Server-backend side
 echo "Creating Client's private key and Certificate Signing Request"
-openssl req -newkey rsa:4096 -nodes -keyout mobile-client-key.pem -out mobile-client-req.pem -subj "/C=PT/ST=Lisbon/L=Oeiras/O=Lemon/OU=Lemon/CN=192.168.1.2/emailAddress=lemon@gmail.pt"
+openssl req -newkey rsa:4096 -nodes -keyout mobile-client-key.pem -out mobile-client-req.pem -subj "/C=PT/ST=Lisbon/L=Oeiras/O=Lemon/OU=Lemon/CN=192.168.1.2/emailAddress=lemon@gmail.pt" -addtext "subjectAltName = IP:192.168.1.2"
 
 echo "Signing client's certificate request with ca's private key"
 openssl x509 -req -in mobile-client-req.pem -CA ca-cert.pem -CAkey ca-key.pem -CAcreateserial -out mobile-client-cert.pem
